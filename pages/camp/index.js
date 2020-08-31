2930dd77790be54f32c2d227fc2962dd6a8622ba
Jay Fallon <jayfallon@gmail.com>
2020-08-31 18:17:41 -0400

    removed .env

---
+++ pages/camp/index.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+const CampStyles = styled.main`
+  padding-top: 80px;
+`;
+
+export default function index() {
+  return (
+    <CampStyles>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia debitis,
+      officia culpa dignissimos placeat, architecto, in error sint velit qui
+      amet saepe libero reiciendis delectus consectetur rerum a sunt molestiae.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut accusamus
+      tempore iusto dicta, autem officiis magnam eligendi soluta fugit illum.
+      Nemo voluptatum id quo officia temporibus nulla sit ipsa distinctio? Lorem
+      ipsum dolor sit amet consectetur adipisicing elit. Cum nesciunt illo iusto
+      ab molestias, dolore vel, quia quas porro expedita quos corrupti.
+      Obcaecati totam nesciunt provident perspiciatis praesentium repellat
+      aspernatur? Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum
+      cumque, corrupti ut molestiae voluptate, consequatur praesentium, nemo non
+      accusantium voluptatum incidunt repudiandae obcaecati minus reiciendis
+      fuga culpa iusto iste suscipit.
+    </CampStyles>
+  );
+}
